d9eff412aa32add2a284e92092afd750d55eb081
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-11-07 20:23:11 +0100

    11 jump

--- Assets/Scripts/PlayerComtroller.cs
+++ Assets/Scripts/PlayerComtroller.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerComtroller : MonoBehaviour
 {
     private float moveSpeed;
+    private float jumpForce;
+
     public Rigidbody2D theRB;
 
     void Start()
     {
         moveSpeed = 10;
+        jumpForce = 15;
     }
 
     void Update()
     {
-        theRB.velocity = new Vector2 (Input.GetAxisRaw("Horitontal") * moveSpeed, theRB.velocity.y);
+        theRB.velocity = new Vector2 (Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
+
+        if(Input.GetButtonDown("Jump")) { 
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
     }
 }
