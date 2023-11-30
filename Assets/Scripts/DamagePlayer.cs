63ccd4858cb29c7215f3dd1737a513b6a60390ca
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-11-30 13:08:55 +0100

    deal damage

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class DamagePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHealthController.Instance.DealDamage(1);
+        }
     }
 }
