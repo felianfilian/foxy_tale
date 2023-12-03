ef10e9042cc47ddfdf5187b76e819553dec4a437
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-12-03 21:08:20 +0100

    ui controller

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
