e48ec063bfdc4df954f2d4773e06b7fefdabe74f
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-12-04 21:14:54 +0100

    update health UI

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,10 +1,14 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
     public static UIController Instance;
+    public UnityEngine.UI.Image[] hearts;
+    public Sprite heartFull, heartHalf, heartEmpty;
 
     private void Awake()
     {
@@ -13,12 +17,20 @@ public class UIController : MonoBehaviour
 
     void Start()
     {
-        
+        UpdateHealthDisplay();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void UpdateHealthDisplay()
+    {
+        for(int i = 0; i < PlayerHealthController.Instance.maxHealth; i++)
+        {
+            hearts[i].sprite = heartEmpty;
+        }
     }
 }
