76e13b568fea09b8361cf733860411d139a43801
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-12-12 22:36:11 +0100

    invincibility

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -7,11 +7,18 @@ public class PlayerHealthController : MonoBehaviour
     public static PlayerHealthController Instance;
 
     public int currentHealth;
+    public float invincibilityLength = 1f;
+
     private int maxHealth;
+    private float invincibilityCounter;
+
+    private SpriteRenderer theSR;
 
     private void Awake()
     {
         Instance = this;
+
+        theSR = GetComponent<SpriteRenderer>();
     }
 
     void Start()
@@ -20,20 +27,35 @@ public class PlayerHealthController : MonoBehaviour
         currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+    if(invincibilityCounter >= 0)
+        {
+            invincibilityCounter -= Time.deltaTime;
+            if(invincibilityCounter <= 0)
+            {
+                theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, 1f);
+            }
+        }    
     }
 
     public void DealDamage(int damage)
     {
-        currentHealth -= damage;
-        if(currentHealth <= 0)
-        {
-            currentHealth = 0;
-            gameObject.SetActive(false);
+        if(invincibilityCounter <= 0) {
+            currentHealth -= damage;
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                invincibilityCounter = invincibilityLength;
+                theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, 0.5f);
+            }
+            UIController.Instance.UpdateHealthDisplay();
         }
-        UIController.Instance.UpdateHealthDisplay();
+        
     }
 }
