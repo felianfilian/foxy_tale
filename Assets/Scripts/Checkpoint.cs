91689e662566a9c64e1434d019c8b78f0073ec4f
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-12-15 21:46:49 +0100

    checkpoint logic

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public SpriteRenderer theSR;
+    public Sprite cpOn;
+    public Sprite cpOff;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.CompareTag("Player"))
+        {
+
+        }
     }
 }
