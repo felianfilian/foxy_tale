b09d6091233de46eaea32f23daa341c80865708d
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-02-12 20:43:58 +0100

    pickup update

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -6,6 +6,7 @@ public class Pickup : MonoBehaviour
 {
     public enum Item { Gem, Health };
     public Item item;
+    public GameObject pickupEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -15,11 +16,13 @@ public class Pickup : MonoBehaviour
             {
                 LevelManager.instance.gemsCollected++;
                 UIController.Instance.UpdateGemCount();
+                Instantiate(pickupEffect, transform.position, transform.rotation);
                 Destroy(gameObject);
             }
             if (item == Item.Health && PlayerHealthController.instance.currentHealth < PlayerHealthController.instance.maxHealth)
             {
                 PlayerHealthController.instance.HealthChange(2);
+                Instantiate(pickupEffect, transform.position, transform.rotation);
                 Destroy(gameObject);
             }
         }
