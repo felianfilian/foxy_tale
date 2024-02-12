cbd9ba645a7291ff638304e855482033e498ed26
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-02-12 06:55:56 +0100

    player killplane

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHealthController.instance.DealDamage(2);
+        }
+    }
+}
