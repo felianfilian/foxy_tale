531a3783bfde6536aed7bbb16ffa261fd2371b2a
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-02-13 21:18:06 +0100

    frog enemy setup

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    public bool isCollected;
+
     public enum Item { Gem, Health };
     public Item item;
     public GameObject pickupEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if(collision.gameObject.CompareTag("Player") && !isCollected)
         {
             if(item == Item.Gem)
             {
                 LevelManager.instance.gemsCollected++;
                 UIController.Instance.UpdateGemCount();
                 Instantiate(pickupEffect, transform.position, transform.rotation);
+                isCollected = true;
                 Destroy(gameObject);
             }
             if (item == Item.Health && PlayerHealthController.instance.currentHealth < PlayerHealthController.instance.maxHealth)
             {
                 PlayerHealthController.instance.HealthChange(2);
                 Instantiate(pickupEffect, transform.position, transform.rotation);
+                isCollected = true;
                 Destroy(gameObject);
             }
         }
