bb2a5d8a0b0cedc98a8b0731389cbcb082d416c2
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-02-14 05:14:11 +0100

    first enemy movement

--- Assets/Scripts/EnemyFrogController.cs
+++ Assets/Scripts/EnemyFrogController.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class EnemyFrogController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public bool movingRight;
 
     public Rigidbody2D theRB;
     public SpriteRenderer theSR;
     public Transform leftPoint, rightPoint;
 
+    private bool movingRight;
+
+    private void Start()
+    {
+        movingRight = true;
+    }
+
     void Update()
     {
-        
+        if(movingRight)
+        {
+            theRB.velocity = new Vector2 (moveSpeed, theRB.velocity.y);
+            theSR.flipX = true;
+        }
     }
 }
